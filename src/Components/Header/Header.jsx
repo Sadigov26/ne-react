dbae465789a4b69fceda447d64524c8fd4fdc716
Kamil Sadigov <101421625+Sadigov26@users.noreply.github.com>
2024-05-16 12:29:04 +0400

    Add files via upload

---
+++ src/Components/Header/Header.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import style from './Header.module.scss'
+
+const Header = () => {
+  return (
+    <div>
+      <header>
+        <div className={style.containerHeader}></div>
+      </header>
+    </div>
+  )
+}
+
+export default Header
---
+++ src/Components/Hero/Hero.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import style from './Hero.module.scss'
+
+const Hero = () => {
+  return (
+    <div>
+      <section  className={style.hero}>
+      <div className={style.containerHeader}></div>
+
+      </section>
+    </div>
+  )
+}
+
+export default Hero
